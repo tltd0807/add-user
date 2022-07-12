ffad631007e56b3b8187710e636019c0c406d942
Dat <todat2001@gmail.com>
2022-07-12 23:31:52 +0700

    add closeModal to overlay div

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,7 +3,7 @@ import Card from "../UI/Card/Card";
 import styles from "./Modal.module.css";
 const Modal = (props) => {
   return (
-    <div className={styles["overlay"]}>
+    <div className={styles["overlay"]} onClick={props.closeModal}>
       <Card className={styles["modal-card"]}>
         <div className={styles["modal-header"]}>INVALID INPUT</div>
         <div className={styles["modal-message"]}>{props.messageError}</div>
